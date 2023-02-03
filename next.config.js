35f9975a580dd4f89f2e662618112888c31e2e5c
dowanna <shun.fzs1000@gmail.com>
2023-02-03 15:36:01 +0900

    fix: image optimization error
    Error: Image Optimization using Next.js' default loader is not compatible with `next export`.
    https://nextjs.org/docs/messages/export-image-api
    これを手っ取り早く解決するため

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
 });
 
 module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  },
 });
