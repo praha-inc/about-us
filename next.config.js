6103b8e1b8523463c89e4c3ed096bddf1524a2a2
dowanna <shun.fzs1000@gmail.com>
2023-02-13 10:57:54 +0900

    replace emoji text to actual utf-8 emojis

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  mdxOptions: {
+    remarkPlugins: [require("remark-emoji")],
+  },
 });
 
 module.exports = withNextra({});
