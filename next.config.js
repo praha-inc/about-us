93b48450416b02e09f8d5e666b5dbd7619abb528
dowanna <shun.fzs1000@gmail.com>
2023-02-17 16:36:45 +0900

    forget emoji conversion

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
-  mdxOptions: {
-    remarkPlugins: [require("remark-emoji")],
-  },
 });
 
 module.exports = withNextra({});
