2c96c6bb7a3d40968773d7bff7f3799a2a3046af
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-08-26 11:20:17 +0900

    chore: Vercelサイトの所有権を証明するための記述の場所が間違っていたので修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,10 @@ import { FC } from 'react'
 const Document: FC = () => {
   return (
     <Html lang="ja">
-      <Head />
-      {/* NOTE: Vercelにホスティングしているサイトの所有権をGoogle Search Consoleに証明するための記述 */}
-      <meta name="google-site-verification" content="5IkFk6t1_lJKoUI7Rofgi6RPwERUS0v6wzKU7eG9RT4" />
+      <Head >
+        {/* NOTE: Vercelにホスティングしているサイトの所有権をGoogle Search Consoleに証明するための記述 */}
+        <meta name="google-site-verification" content="5IkFk6t1_lJKoUI7Rofgi6RPwERUS0v6wzKU7eG9RT4" />
+      </Head>
       <body>
         <Main />
         <NextScript />
