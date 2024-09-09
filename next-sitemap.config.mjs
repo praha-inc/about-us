3010bc9a0b95ae3402d9e8a9e1e6d15c50add3ba
Sakiyama Shingo <yamaa.exception@gmail.com>
2024-09-09 20:04:56 +0900

    chore: sitemap.xmlとrobots.txtが生成されるように変更

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+  siteUrl: 'https://entrance.praha-inc.com/',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: './out',
+};
